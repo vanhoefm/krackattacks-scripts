6e33ca4529e7b4eae9a46db224282a116d112ffa
Jouni Malinen <j@w1.fi>
2008-07-15 20:05:50 +0300

    Fixed wpa_scan_get_max_rate() to clear the basicrate flag when determining the highest rate.

--- src/drivers/scan_helpers.c
+++ src/drivers/scan_helpers.c
@@ -67,14 +67,14 @@ int wpa_scan_get_max_rate(const struct wpa_scan_res *res)
 
 	ie = wpa_scan_get_ie(res, WLAN_EID_SUPP_RATES);
 	for (i = 0; ie && i < ie[1]; i++) {
-		if (ie[i + 2] > rate)
-			rate = ie[i + 2];
+		if ((ie[i + 2] & 0x7f) > rate)
+			rate = ie[i + 2] & 0x7f;
 	}
 
 	ie = wpa_scan_get_ie(res, WLAN_EID_EXT_SUPP_RATES);
 	for (i = 0; ie && i < ie[1]; i++) {
-		if (ie[i + 2] > rate)
-			rate = ie[i + 2];
+		if ((ie[i + 2] & 0x7f) > rate)
+			rate = ie[i + 2] & 0x7f;
 	}
 
 	return rate;

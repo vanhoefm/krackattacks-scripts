953f83439b2f0f22e0a0182de9e9fbc42ce09d13
Jouni Malinen <j@w1.fi>
2009-12-05 21:14:08 +0200

    Move Milenage test code into the new tests directory

--- src/crypto/milenage.h
+++ src/crypto/milenage.h
@@ -25,5 +25,9 @@ int gsm_milenage(const u8 *opc, const u8 *k, const u8 *_rand, u8 *sres,
 int milenage_check(const u8 *opc, const u8 *k, const u8 *sqn, const u8 *_rand,
 		   const u8 *autn, u8 *ik, u8 *ck, u8 *res, size_t *res_len,
 		   u8 *auts);
+int milenage_f1(const u8 *opc, const u8 *k, const u8 *_rand,
+		const u8 *sqn, const u8 *amf, u8 *mac_a, u8 *mac_s);
+int milenage_f2345(const u8 *opc, const u8 *k, const u8 *_rand,
+		   u8 *res, u8 *ck, u8 *ik, u8 *ak, u8 *akstar);
 
 #endif /* MILENAGE_H */

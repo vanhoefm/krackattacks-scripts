05ba8690cb296a83e0823553bcc7f7b5f8d5db30
Jouni Malinen <j@w1.fi>
2010-03-06 22:37:48 +0200

    nl80211: Unregister forgotten eloop socket on init failure

--- src/drivers/driver_nl80211.c
+++ src/drivers/driver_nl80211.c
@@ -1293,6 +1293,7 @@ failed:
 	nl_cache_free(drv->nl_cache);
 	nl_handle_destroy(drv->nl_handle);
 	nl_cb_put(drv->nl_cb);
+	eloop_unregister_read_sock(nl_socket_get_fd(drv->nl_handle_event));
 
 	os_free(drv);
 	return NULL;

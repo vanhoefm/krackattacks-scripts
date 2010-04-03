f8130b07bbf62cdbe3c5fc4b650dc4ded20607de
Jouni Malinen <j@w1.fi>
2010-04-03 09:14:29 -0700

    driver_osx: Update set_key arguments to fix build

--- src/drivers/driver_osx.m
+++ src/drivers/driver_osx.m
@@ -303,7 +303,8 @@ static int wpa_driver_osx_associate(void *priv,
 }
 
 
-static int wpa_driver_osx_set_key(const char *ifname, void *priv, wpa_alg alg, const u8 *addr,
+static int wpa_driver_osx_set_key(const char *ifname, void *priv,
+				  enum wpa_alg alg, const u8 *addr,
 				  int key_idx, int set_tx, const u8 *seq,
 				  size_t seq_len, const u8 *key,
 				  size_t key_len)

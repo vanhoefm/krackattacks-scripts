bf0ed63f3fa3e026370b518a4d7f574244f3c660
Jouni Malinen <jouni.malinen@atheros.com>
2010-10-07 10:51:04 +0300

    Allow a postfix to be defined for the version number
    A separate build number (etc.) version number postfix can now be
    added to the build without having to modify source code files by
    defining VERSION_STR_POSTFIX. This can be done, e.g., by adding
    following line to .config:
    
    CFLAGS += -DVERSION_STR_POSTFIX=\"-foo\"

--- src/common/version.h
+++ src/common/version.h
@@ -1,6 +1,10 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define VERSION_STR "0.8.x"
+#ifndef VERSION_STR_POSTFIX
+#define VERSION_STR_POSTFIX ""
+#endif /* VERSION_STR_POSTFIX */
+
+#define VERSION_STR "0.8.x" VERSION_STR_POSTFIX
 
 #endif /* VERSION_H */

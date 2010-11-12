f3b87561d7614f4b4ea2aea13400a249225c7fee
Jouni Malinen <j@w1.fi>
2010-11-12 21:52:14 +0200

    Share WPA IE parser function for RSN authenticator/supplicant
    There is no point in maintaining two almost identical versions
    of this parser. Move WPA IE parser into wpa_common.c similarly
    to what was already the case with RSN IE parse.

--- src/common/wpa_common.h
+++ src/common/wpa_common.h
@@ -337,6 +337,8 @@ struct wpa_ie_data {
 
 int wpa_parse_wpa_ie_rsn(const u8 *rsn_ie, size_t rsn_ie_len,
 			 struct wpa_ie_data *data);
+int wpa_parse_wpa_ie_wpa(const u8 *wpa_ie, size_t wpa_ie_len,
+			 struct wpa_ie_data *data);
 
 void rsn_pmkid(const u8 *pmk, size_t pmk_len, const u8 *aa, const u8 *spa,
 	       u8 *pmkid, int use_sha256);

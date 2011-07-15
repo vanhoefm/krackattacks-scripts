e1d526293b27f5123777e695750a0d38bdfe936a
Jouni Malinen <j@w1.fi>
2011-07-15 21:49:50 +0300

    P2P: Do not reply to Probe Request frame indicating only 802.11b rates
    Per P2P specification 2.4.1, P2P Device shall shall not respond to
    Probe Request frames that indicate support for only 802.11b rates.

--- src/p2p/p2p.c
+++ src/p2p/p2p.c
@@ -1644,6 +1644,31 @@ struct wpabuf * p2p_build_probe_resp_ies(struct p2p_data *p2p)
 }
 
 
+static int is_11b(u8 rate)
+{
+	return rate == 0x02 || rate == 0x04 || rate == 0x0b || rate == 0x16;
+}
+
+
+static int supp_rates_11b_only(struct ieee802_11_elems *elems)
+{
+	int num_11b = 0, num_others = 0;
+	int i;
+
+	if (elems->supp_rates == NULL && elems->ext_supp_rates == NULL)
+		return 0;
+
+	for (i = 0; elems->supp_rates && i < elems->supp_rates_len; i++) {
+		if (is_11b(elems->supp_rates[i]))
+			num_11b++;
+		else
+			num_others++;
+	}
+
+	return num_11b > 0 && num_others == 0;
+}
+
+
 static void p2p_reply_probe(struct p2p_data *p2p, const u8 *addr,
 			    const u8 *dst, const u8 *bssid, const u8 *ie,
 			    size_t ie_len)
@@ -1686,6 +1711,11 @@ static void p2p_reply_probe(struct p2p_data *p2p, const u8 *addr,
 	    os_memcmp(elems.ssid, P2P_WILDCARD_SSID, P2P_WILDCARD_SSID_LEN) !=
 	    0) {
 		/* not using P2P Wildcard SSID - ignore */
+		return;
+	}
+
+	if (supp_rates_11b_only(&elems)) {
+		/* Indicates support for 11b rates only */
 		return;
 	}
 

2683690d9139b595607523cc87b16858ec785b2a
Jouni Malinen <j@w1.fi>
2011-10-23 12:33:17 +0300

    Fix hostapd_wpa_auth_send_ether() return value
    This was not currently used for anything, but better return the correct
    value instead of hardcoded -1.

--- src/ap/wpa_auth_glue.c
+++ src/ap/wpa_auth_glue.c
@@ -384,7 +384,7 @@ static int hostapd_wpa_auth_send_ether(void *ctx, const u8 *dst, u16 proto,
 	ret = l2_packet_send(hapd->l2, dst, proto, (u8 *) buf,
 			     sizeof(*buf) + data_len);
 	os_free(buf);
-	return -1;
+	return ret;
 }
 
 

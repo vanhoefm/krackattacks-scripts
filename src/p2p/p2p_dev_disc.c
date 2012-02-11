e22d4d957bea049a37dfc2697b895c8ff6cd8b1c
Jouni Malinen <j@w1.fi>
2012-02-11 16:31:36 +0200

    Remove the GPL notification from files contributed by Atheros
    Remove the GPL notification text from files that were initially
    contributed by Atheros Communications or Qualcomm Atheros.
    
    Signed-hostap: Jouni Malinen <j@w1.fi>

--- src/p2p/p2p_dev_disc.c
+++ src/p2p/p2p_dev_disc.c
@@ -2,14 +2,8 @@
  * Wi-Fi Direct - P2P Device Discoverability procedure
  * Copyright (c) 2010, Atheros Communications
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 as
- * published by the Free Software Foundation.
- *
- * Alternatively, this software may be distributed under the terms of BSD
- * license.
- *
- * See README and COPYING for more details.
+ * This software may be distributed under the terms of the BSD license.
+ * See README for more details.
  */
 
 #include "includes.h"
--- wpa_supplicant/gas_query.h
+++ wpa_supplicant/gas_query.h
@@ -3,14 +3,8 @@
  * Copyright (c) 2009, Atheros Communications
  * Copyright (c) 2011, Qualcomm Atheros
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 as
- * published by the Free Software Foundation.
- *
- * Alternatively, this software may be distributed under the terms of BSD
- * license.
- *
- * See README and COPYING for more details.
+ * This software may be distributed under the terms of the BSD license.
+ * See README for more details.
  */
 
 #ifndef GAS_QUERY_H

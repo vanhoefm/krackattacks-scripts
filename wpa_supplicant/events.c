cb6710a4a5725c036b69ff751545c32d37f2e7de
Jouni Malinen <j@w1.fi>
2012-02-18 13:16:17 +0200

    Use WPA_INTERFACE_DISABLED with interface removed events
    This makes WPA_INTERFACE_DISABLED more consistent in indicating that
    wpa_supplicant cannot currently control the interface regardless of
    whether the interface is disabled or completely removed.
    
    Signed-hostap: Jouni Malinen <j@w1.fi>
    intended-for: hostap-1

--- wpa_supplicant/events.c
+++ wpa_supplicant/events.c
@@ -1820,11 +1820,13 @@ wpa_supplicant_event_interface_status(struct wpa_supplicant *wpa_s,
 			wpa_msg(wpa_s, MSG_INFO, "Failed to initialize the "
 				"driver after interface was added");
 		}
+		wpa_supplicant_set_state(wpa_s, WPA_DISCONNECTED);
 		break;
 	case EVENT_INTERFACE_REMOVED:
 		wpa_dbg(wpa_s, MSG_DEBUG, "Configured interface was removed");
 		wpa_s->interface_removed = 1;
 		wpa_supplicant_mark_disassoc(wpa_s);
+		wpa_supplicant_set_state(wpa_s, WPA_INTERFACE_DISABLED);
 		l2_packet_deinit(wpa_s->l2);
 		wpa_s->l2 = NULL;
 #ifdef CONFIG_IBSS_RSN

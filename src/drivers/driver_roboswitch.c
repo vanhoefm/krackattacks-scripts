cb2b381169bf0f0675a0aa28abae668f6bab45c1
Jouni Malinen <j@w1.fi>
2012-07-02 12:43:16 +0300

    Update license notification in roboswitch interface file
    This updates the license notification to use only the BSD license. The
    changes were acknowledged by email (Jouke Witteveen
    <j.witteveen@gmail.com>, Mon, 2 Jul 2012 11:38:34 +0200).
    
    Signed-hostap: Jouni Malinen <j@w1.fi>

--- src/drivers/driver_roboswitch.c
+++ src/drivers/driver_roboswitch.c
@@ -2,14 +2,8 @@
  * WPA Supplicant - roboswitch driver interface
  * Copyright (c) 2008-2009 Jouke Witteveen
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 as
- * published by the Free Software Foundation.
- *
- * Alternatively, this software may be distributed under the terms of BSD
- * license.
- *
- * See README and COPYING for more details.
+ * This software may be distributed under the terms of the BSD license.
+ * See README for more details.
  */
 
 #include "includes.h"

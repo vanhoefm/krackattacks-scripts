623ecdd5a28bafbac97786c3fe667045d3d78c5d
Jouni Malinen <j@w1.fi>
2012-08-12 11:36:45 +0300

    Remove default ACM=1 from AC_VO and AC_VI
    There is no need to mandate admission control (ACM=1) by default, so
    clear that flag in the case the configuration file does not specify
    wmm_ac_{vo,vi}_acm value. This gets closer to the values mentioned
    in the sample hostapd.conf file and reduces the need to specify WMM
    parameters in the configuration file for most common use cases.
    
    Signed-hostap: Jouni Malinen <j@w1.fi>

--- wpa_supplicant/config.c
+++ wpa_supplicant/config.c
@@ -2555,9 +2555,9 @@ struct wpa_config * wpa_config_alloc_empty(const char *ctrl_interface,
 	const struct hostapd_wmm_ac_params ac_be =
 		{ aCWmin, aCWmax, 3, 0, 0 }; /* best effort traffic */
 	const struct hostapd_wmm_ac_params ac_vi = /* video traffic */
-		{ aCWmin - 1, aCWmin, 2, 3000 / 32, 1 };
+		{ aCWmin - 1, aCWmin, 2, 3000 / 32, 0 };
 	const struct hostapd_wmm_ac_params ac_vo = /* voice traffic */
-		{ aCWmin - 2, aCWmin - 1, 2, 1500 / 32, 1 };
+		{ aCWmin - 2, aCWmin - 1, 2, 1500 / 32, 0 };
 
 	config = os_zalloc(sizeof(*config));
 	if (config == NULL)

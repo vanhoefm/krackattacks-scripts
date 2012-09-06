44256451130c4766e4a019162de17d0734444ee9
Arik Nemtsov <arik@wizery.com>
2012-09-07 00:22:40 +0300

    AP: Configure basic rates from iface and not conf
    The conf doesn't contain any basic rates in some cases. Most notably,
    when starting a P2P GO in 5 GHz. Use the iface rates which are
    initialized in hostapd_prepare_rates() to the conf rates or set to
    default values if no conf values exist. This fixes a bug introduced in
    commit e5693c4775bae65faa960f80889f98b0a6cb2e1c.
    
    Signed-hostap: Arik Nemtsov <arik@wizery.com>

--- src/ap/beacon.c
+++ src/ap/beacon.c
@@ -668,7 +668,7 @@ void ieee802_11_set_beacon(struct hostapd_data *hapd)
 	params.proberesp_len = resp_len;
 	params.dtim_period = hapd->conf->dtim_period;
 	params.beacon_int = hapd->iconf->beacon_int;
-	params.basic_rates = hapd->iconf->basic_rates;
+	params.basic_rates = hapd->iface->basic_rates;
 	params.ssid = hapd->conf->ssid.ssid;
 	params.ssid_len = hapd->conf->ssid.ssid_len;
 	params.pairwise_ciphers = hapd->conf->rsn_pairwise ?

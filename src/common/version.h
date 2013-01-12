22760dd94722a61175ff90c59d88c4cda1ed5e23
Jouni Malinen <j@w1.fi>
2013-01-12 17:42:53 +0200

    Prepare for hostapd/wpa_supplicant v2.0 release
    Signed-hostap: Jouni Malinen <j@w1.fi>

--- src/common/version.h
+++ src/common/version.h
@@ -5,6 +5,6 @@
 #define VERSION_STR_POSTFIX ""
 #endif /* VERSION_STR_POSTFIX */
 
-#define VERSION_STR "2.0-devel" VERSION_STR_POSTFIX
+#define VERSION_STR "2.0" VERSION_STR_POSTFIX
 
 #endif /* VERSION_H */

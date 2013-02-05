cc03d0fef3bf5913f8e11b7e998c10bf36a3c07f
Amar Singhal <asinghal@qca.qualcomm.com>
2013-02-06 00:31:34 +0200

    Add "BSS LAST ..." command
    Add the "BSS LAST ..." command. This command helps in fetching the scan
    entries iteratively from FIRST entry to LAST entry by ID.
    
    Signed-hostap: Jouni Malinen <jouni@qca.qualcomm.com>

--- wpa_supplicant/ctrl_iface.c
+++ wpa_supplicant/ctrl_iface.c
@@ -3175,6 +3175,8 @@ static int wpa_supplicant_ctrl_iface_bss(struct wpa_supplicant *wpa_s,
 		}
 	} else if (os_strncmp(cmd, "FIRST", 5) == 0)
 		bss = dl_list_first(&wpa_s->bss_id, struct wpa_bss, list_id);
+	else if (os_strncmp(cmd, "LAST", 4) == 0)
+		bss = dl_list_last(&wpa_s->bss_id, struct wpa_bss, list_id);
 	else if (os_strncmp(cmd, "ID-", 3) == 0) {
 		i = atoi(cmd + 3);
 		bss = wpa_bss_get_id(wpa_s, i);

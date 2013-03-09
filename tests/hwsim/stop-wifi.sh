8b2b687eb87b385202e62a2e460f64e50f778ce6
Jouni Malinen <j@w1.fi>
2013-03-09 17:39:11 +0200

    tests: Start wlantest on hwsim0
    This is in preparation for using wlantest to verify correct behavior in
    various test cases.
    
    Signed-hostap: Jouni Malinen <j@w1.fi>

--- tests/hwsim/stop-wifi.sh
+++ tests/hwsim/stop-wifi.sh
@@ -2,6 +2,10 @@
 
 sudo killall -q hostapd
 sudo killall -q wpa_supplicant
+sudo killall -q wlantest
+if grep -q hwsim0 /proc/net/dev; then
+    sudo ifconfig hwsim0 down
+fi
 if grep -q mac80211_hwsim /proc/modules ; then
     sudo rmmod mac80211_hwsim 
 fi

ba873c12845b620be6405ece2512fe8d9691b8f6
Felix Fietkau <nbd@openwrt.org>
2013-03-16 12:35:49 +0200

    hostapd: Fix client reassociation after disconnect due to ACK failure
    Clear WLAN_STA_ASSOC_REQ_OK, otherwise no Class 3 frame will be sent to
    the disconnected STA in response to data frames.
    
    Signed-hostap: Felix Fietkau <nbd@openwrt.org>

--- src/ap/sta_info.c
+++ src/ap/sta_info.c
@@ -576,7 +576,7 @@ void ap_sta_disassociate(struct hostapd_data *hapd, struct sta_info *sta,
 {
 	wpa_printf(MSG_DEBUG, "%s: disassociate STA " MACSTR,
 		   hapd->conf->iface, MAC2STR(sta->addr));
-	sta->flags &= ~WLAN_STA_ASSOC;
+	sta->flags &= ~(WLAN_STA_ASSOC | WLAN_STA_ASSOC_REQ_OK);
 	ap_sta_set_authorized(hapd, sta, 0);
 	sta->timeout_next = STA_DEAUTH;
 	wpa_printf(MSG_DEBUG, "%s: reschedule ap_handle_timer timeout "

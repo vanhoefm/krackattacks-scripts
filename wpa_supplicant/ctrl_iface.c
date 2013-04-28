a8a7890d7c74fd41f2c4e8d57c1c76d0788f2e4f
Jouni Malinen <j@w1.fi>
2013-04-28 22:35:15 +0300

    Clear extra_blacklist_count on FLUSH command
    This is needed to avoid old blacklist failure counts from being valid
    after state flush.
    
    Signed-hostap: Jouni Malinen <j@w1.fi>

--- wpa_supplicant/ctrl_iface.c
+++ wpa_supplicant/ctrl_iface.c
@@ -5059,6 +5059,7 @@ static void wpa_supplicant_ctrl_iface_flush(struct wpa_supplicant *wpa_s)
 
 	wpa_bss_flush(wpa_s);
 	wpa_blacklist_clear(wpa_s);
+	wpa_s->extra_blacklist_count = 0;
 	wpa_supplicant_ctrl_iface_remove_network(wpa_s, "all");
 	wpa_supplicant_ctrl_iface_remove_cred(wpa_s, "all");
 }

76d320ef3f0cba06c4565ae231dc3f28de060020
Jouni Malinen <j@w1.fi>
2013-08-24 19:01:32 +0300

    tests: Update concurrent P2P setup to match start.sh changes
    The design for launching AP mode interfaces was changed, but
    start-p2p-concurrent.sh was not updated with matching changes.
    
    Signed-hostap: Jouni Malinen <j@w1.fi>

--- tests/hwsim/start-p2p-concurrent.sh
+++ tests/hwsim/start-p2p-concurrent.sh
@@ -2,10 +2,11 @@
 
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 WPAS=$DIR/../../wpa_supplicant/wpa_supplicant
+HAPD=$DIR/../../hostapd/hostapd
 WLANTEST=$DIR/../../wlantest/wlantest
 
 $DIR/stop-wifi.sh
-sudo modprobe mac80211_hwsim radios=3
+sudo modprobe mac80211_hwsim radios=5
 sudo iw wlan0 interface add sta0 type station
 sudo iw wlan1 interface add sta1 type station
 sudo iw wlan2 interface add sta2 type station
@@ -13,7 +14,10 @@ mkdir -p $DIR/logs
 DATE=`date +%s`
 sudo ifconfig hwsim0 up
 sudo $WLANTEST -i hwsim0 -c -d > $DIR/logs/$DATE-hwsim0 &
-sudo $WPAS -Dnl80211 -iwlan0 -c $DIR/p2p0.conf -N -Dnl80211 -ista0 -c $DIR/sta-dummy.conf -ddKt > $DIR/logs/$DATE-log0 &
-sudo $WPAS -Dnl80211 -iwlan1 -c $DIR/p2p1.conf -N -Dnl80211 -ista1 -c $DIR/sta-dummy.conf -ddKt > $DIR/logs/$DATE-log1 &
-sudo $WPAS -Dnl80211 -iwlan2 -c $DIR/p2p2.conf -N -Dnl80211 -ista2 -c $DIR/sta-dummy.conf -ddKt > $DIR/logs/$DATE-log2 &
+sudo tcpdump -ni hwsim0 -s 2500 -w $DIR/logs/$DATE-hwsim0.dump &
+sudo $WPAS -g /tmp/wpas-wlan0 -Gadmin -Dnl80211 -iwlan0 -c $DIR/p2p0.conf -N -Dnl80211 -ista0 -c $DIR/sta-dummy.conf -ddKt > $DIR/logs/$DATE-log0 &
+sudo $WPAS -g /tmp/wpas-wlan1 -Gadmin -Dnl80211 -iwlan1 -c $DIR/p2p1.conf -N -Dnl80211 -ista1 -c $DIR/sta-dummy.conf -ddKt > $DIR/logs/$DATE-log1 &
+sudo $WPAS -g /tmp/wpas-wlan2 -Gadmin -Dnl80211 -iwlan2 -c $DIR/p2p2.conf -N -Dnl80211 -ista2 -c $DIR/sta-dummy.conf -ddKt > $DIR/logs/$DATE-log2 &
+sudo $HAPD -ddKt -g /var/run/hostapd-global -G admin -ddKt > $DIR/logs/$DATE-hostapd &
 sleep 1
+sudo chown $USER $DIR/logs/$DATE-hwsim0.dump

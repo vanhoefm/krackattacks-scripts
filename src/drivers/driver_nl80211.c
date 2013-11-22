982189632fe8ef4a885a82257cd90e0dc26a17ce
Dmitry Shmidt <dimitrysh@google.com>
2013-11-22 12:39:07 -0800

    Android: Revert static field back to send_and_recv_msgs()
    Signed-off-by: Dmitry Shmidt <dimitrysh@google.com>

--- src/drivers/driver_nl80211.c
+++ src/drivers/driver_nl80211.c
@@ -625,13 +625,10 @@ static int send_and_recv_msgs_global(struct nl80211_global *global,
 }
 
 
-#ifndef ANDROID_P2P
-static
-#endif /* ANDROID_P2P */
-int send_and_recv_msgs(struct wpa_driver_nl80211_data *drv,
-		       struct nl_msg *msg,
-		       int (*valid_handler)(struct nl_msg *, void *),
-		       void *valid_data)
+static int send_and_recv_msgs(struct wpa_driver_nl80211_data *drv,
+			      struct nl_msg *msg,
+			      int (*valid_handler)(struct nl_msg *, void *),
+			      void *valid_data)
 {
 	return send_and_recv(drv->global, drv->global->nl, msg,
 			     valid_handler, valid_data);

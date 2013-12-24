5f9c134ab4fd6923249aef54d045143f7756c71b
Jouni Malinen <j@w1.fi>
2013-12-24 22:59:52 +0200

    Remove obsolete license notifications
    These files have been distributed only under the BSD license option
    since February 2012. Clarify the license statements in the files to
    match that to avoid confusion.
    
    Signed-hostap: Jouni Malinen <j@w1.fi>

--- src/ap/beacon.h
+++ src/ap/beacon.h
@@ -3,14 +3,8 @@
  * Copyright (c) 2002-2004, Instant802 Networks, Inc.
  * Copyright (c) 2005-2006, Devicescape Software, Inc.
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 as
- * published by the Free Software Foundation.
- *
- * Alternatively, this software may be distributed under the terms of BSD
- * license.
- *
- * See README and COPYING for more details.
+ * This software may be distributed under the terms of the BSD license.
+ * See README for more details.
  */
 
 #ifndef BEACON_H

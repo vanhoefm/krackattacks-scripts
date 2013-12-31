2e3aec56e7829e4d0a9563ecf25824e57cb1e842
Jouni Malinen <j@w1.fi>
2013-12-31 11:32:48 +0200

    tests: Verify mixed mode WPA+WPA2 operation
    Signed-hostap: Jouni Malinen <j@w1.fi>

--- tests/hwsim/test_ap_ciphers.py
+++ tests/hwsim/test_ap_ciphers.py
@@ -46,3 +46,45 @@ def test_ap_cipher_ccmp_256(dev, apdev):
 def test_ap_cipher_gcmp_256(dev, apdev):
     """WPA2-PSK/GCMP-256 connection"""
     check_cipher(dev[0], apdev[0], "GCMP-256")
+
+def test_ap_cipher_mixed_wpa_wpa2(dev, apdev):
+    """WPA2-PSK/CCMP/ and WPA-PSK/TKIP mixed configuration"""
+    ssid = "test-wpa-wpa2-psk"
+    passphrase = "12345678"
+    params = { "ssid": ssid,
+               "wpa_passphrase": passphrase,
+               "wpa": "3",
+               "wpa_key_mgmt": "WPA-PSK",
+               "rsn_pairwise": "CCMP",
+               "wpa_pairwise": "TKIP" }
+    hostapd.add_ap(apdev[0]['ifname'], params)
+    dev[0].request("SET ignore_old_scan_res 1")
+    dev[0].connect(ssid, psk=passphrase, proto="WPA2",
+                   pairwise="CCMP", group="TKIP", scan_freq="2412")
+    status = dev[0].get_status()
+    if status['key_mgmt'] != 'WPA2-PSK':
+        raise Exception("Incorrect key_mgmt reported")
+    if status['pairwise_cipher'] != 'CCMP':
+        raise Exception("Incorrect pairwise_cipher reported")
+    if status['group_cipher'] != 'TKIP':
+        raise Exception("Incorrect group_cipher reported")
+    bss = dev[0].get_bss(apdev[0]['bssid'])
+    if bss['ssid'] != ssid:
+        raise Exception("Unexpected SSID in the BSS entry")
+    if "[WPA-PSK-TKIP]" not in bss['flags']:
+        raise Exception("Missing BSS flag WPA-PSK-TKIP")
+    if "[WPA2-PSK-CCMP]" not in bss['flags']:
+        raise Exception("Missing BSS flag WPA2-PSK-CCMP")
+    hwsim_utils.test_connectivity(dev[0].ifname, apdev[0]['ifname'])
+
+    dev[1].connect(ssid, psk=passphrase, proto="WPA",
+                   pairwise="TKIP", group="TKIP", scan_freq="2412")
+    status = dev[1].get_status()
+    if status['key_mgmt'] != 'WPA-PSK':
+        raise Exception("Incorrect key_mgmt reported")
+    if status['pairwise_cipher'] != 'TKIP':
+        raise Exception("Incorrect pairwise_cipher reported")
+    if status['group_cipher'] != 'TKIP':
+        raise Exception("Incorrect group_cipher reported")
+    hwsim_utils.test_connectivity(dev[1].ifname, apdev[0]['ifname'])
+    hwsim_utils.test_connectivity(dev[0].ifname, dev[1].ifname)

7b2c42f8b942842438fb4c0ab3c1134f4a637676
Jouni Malinen <jouni@qca.qualcomm.com>
2014-01-23 10:24:21 +0200

    hostapd: Fix PMF robust Action frame processing rules
    Due to misplaced parenthesis, unprotected not-Robust Action frames
    (e.g., Public Action frames) were dropped in handle_assoc() when such
    frames were received during an association that had negotiated use of
    PMF. This is not correct since only unprotected Robust Action frames
    were supposed to be handled in this way.
    
    This would have broken any Public Action frame use during PMF
    association, but such frames were not really supposed to be used
    currently (ANQP as the only possible use case should really use
    protected dual option in such case).
    
    Signed-hostap: Jouni Malinen <jouni@qca.qualcomm.com>

--- src/ap/ieee802_11.c
+++ src/ap/ieee802_11.c
@@ -1588,8 +1588,8 @@ static int handle_action(struct hostapd_data *hapd,
 
 #ifdef CONFIG_IEEE80211W
 	if (sta && (sta->flags & WLAN_STA_MFP) &&
-	    !(mgmt->frame_control & host_to_le16(WLAN_FC_ISWEP) &&
-	      robust_action_frame(mgmt->u.action.category))) {
+	    !(mgmt->frame_control & host_to_le16(WLAN_FC_ISWEP)) &&
+	    robust_action_frame(mgmt->u.action.category)) {
 		hostapd_logger(hapd, mgmt->sa, HOSTAPD_MODULE_IEEE80211,
 			       HOSTAPD_LEVEL_DEBUG,
 			       "Dropped unprotected Robust Action frame from "

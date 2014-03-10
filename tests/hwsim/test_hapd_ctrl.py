fe23f3816ea86c79797801034477c3520af22b1f
Jouni Malinen <jouni@qca.qualcomm.com>
2014-03-10 10:58:01 +0200

    tests: hostapd as P2P Device manager
    Signed-off-by: Jouni Malinen <jouni@qca.qualcomm.com>

--- tests/hwsim/test_hapd_ctrl.py
+++ tests/hwsim/test_hapd_ctrl.py
@@ -28,3 +28,19 @@ def test_hapd_ctrl_status(dev, apdev):
         raise Exception("Unexpected beacon_set")
     if driver['addr'] != bssid:
         raise Exception("Unexpected addr")
+
+def test_hapd_ctrl_p2p_manager(dev, apdev):
+    """hostapd as P2P Device manager"""
+    ssid = "hapd-p2p-mgr"
+    passphrase = "12345678"
+    params = hostapd.wpa2_params(ssid=ssid, passphrase=passphrase)
+    params['manage_p2p'] = '1'
+    params['allow_cross_connection'] = '0'
+    hapd = hostapd.add_ap(apdev[0]['ifname'], params)
+    dev[0].connect(ssid, psk=passphrase, scan_freq="2412")
+    addr = dev[0].p2p_dev_addr()
+    if "OK" not in hapd.request("DEAUTHENTICATE " + addr + " p2p=2"):
+        raise Exception("DEAUTHENTICATE command failed")
+    ev = dev[0].wait_event(["CTRL-EVENT-DISCONNECTED"], timeout=5)
+    if ev is None:
+        raise Exception("Disconnection event timed out")

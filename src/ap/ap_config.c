f4e3860f8a770a0db3816196c77baf894c7ccc1e
Jouni Malinen <jouni@qca.qualcomm.com>
2014-03-17 18:23:13 +0200

    Fix AP mode default TXOP Limit values for AC_VI and AC_VO
    These were previous set to 3.0 and 1.5 ms which ended up using values 93
    and 46 in 36 usec inits. However, the default values for these are
    actually defined as 3.008 ms and 1.504 ms (94/47) and those values are
    also listed in the hostapd.conf example.
    
    Signed-off-by: Jouni Malinen <jouni@qca.qualcomm.com>

--- src/ap/ap_config.c
+++ src/ap/ap_config.c
@@ -107,9 +107,9 @@ struct hostapd_config * hostapd_config_defaults(void)
 	const struct hostapd_wmm_ac_params ac_be =
 		{ aCWmin, aCWmax, 3, 0, 0 }; /* best effort traffic */
 	const struct hostapd_wmm_ac_params ac_vi = /* video traffic */
-		{ aCWmin - 1, aCWmin, 2, 3000 / 32, 0 };
+		{ aCWmin - 1, aCWmin, 2, 3008 / 32, 0 };
 	const struct hostapd_wmm_ac_params ac_vo = /* voice traffic */
-		{ aCWmin - 2, aCWmin - 1, 2, 1500 / 32, 0 };
+		{ aCWmin - 2, aCWmin - 1, 2, 1504 / 32, 0 };
 	const struct hostapd_tx_queue_params txq_bk =
 		{ 7, ecw2cw(aCWmin), ecw2cw(aCWmax), 0 };
 	const struct hostapd_tx_queue_params txq_be =

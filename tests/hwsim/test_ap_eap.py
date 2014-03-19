5c65e277a0dfbd09a5e89a8b9334599e966a46c9
Jouni Malinen <j@w1.fi>
2014-03-19 23:46:53 +0200

    tests: Increase altsubject_match coverage
    Signed-off-by: Jouni Malinen <j@w1.fi>

--- tests/hwsim/test_ap_eap.py
+++ tests/hwsim/test_ap_eap.py
@@ -171,7 +171,8 @@ def test_ap_wpa2_eap_ttls_chap(dev, apdev):
     hostapd.add_ap(apdev[0]['ifname'], params)
     eap_connect(dev[0], apdev[0], "TTLS", "chap user",
                 anonymous_identity="ttls", password="password",
-                ca_cert="auth_serv/ca.der", phase2="auth=CHAP")
+                ca_cert="auth_serv/ca.der", phase2="auth=CHAP",
+                altsubject_match="EMAIL:noone@example.com;URI:http://example.com/;DNS:server.w1.fi")
     hwsim_utils.test_connectivity(dev[0].ifname, apdev[0]['ifname'])
     eap_reauth(dev[0], "TTLS")
 

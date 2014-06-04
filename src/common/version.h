b63b9a7bc902836447f96bf9fd6532e8dc0a1391
Jouni Malinen <j@w1.fi>
2014-06-04 16:09:59 +0300

    Change version information for the 2.2 release
    Signed-off-by: Jouni Malinen <j@w1.fi>

--- src/common/version.h
+++ src/common/version.h
@@ -5,6 +5,6 @@
 #define VERSION_STR_POSTFIX ""
 #endif /* VERSION_STR_POSTFIX */
 
-#define VERSION_STR "2.2-devel" VERSION_STR_POSTFIX
+#define VERSION_STR "2.2" VERSION_STR_POSTFIX
 
 #endif /* VERSION_H */

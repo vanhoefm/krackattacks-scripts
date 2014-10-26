102c2bf46185d334a046682d30d40f783856ac3e
Ilan Peer <ilan.peer@intel.com>
2014-10-26 03:06:28 -0400

    tests: Add an option to build with code coverage
    "./build.sh --codecov" can now be used to request CONFIG_CODE_COVERAGE=y
    to be added to hostapd and wpa_supplicant .config files.
    
    Signed-off-by: Ilan Peer <ilan.peer@intel.com>

--- tests/hwsim/build.sh
+++ tests/hwsim/build.sh
@@ -4,16 +4,50 @@ set -e
 
 cd $(dirname $0)
 
+use_lcov=0
+while [ "$1" != "" ]; do
+	case $1 in
+		-c | --codecov ) shift
+			echo "$0: use code coverage specified"
+			use_lcov=1
+			;;
+		* ) exit 1
+	esac
+done
+
 cd ../../wpa_supplicant
 if [ ! -e .config ]; then
     cp ../tests/hwsim/example-wpa_supplicant.config .config
+else
+    echo "wpa_supplicant config file exists"
 fi
+
+if [ $use_lcov -eq 1 ]; then
+    if ! grep -q CONFIG_CODE_COVERAGE .config; then
+	    echo CONFIG_CODE_COVERAGE=y >> .config
+    else
+	    echo "CONFIG_CODE_COVERAGE already exists in wpa_supplicant/.config. Ignore"
+    fi
+fi
+
 make clean
 make -j8
+
 cd ../hostapd
 if [ ! -e .config ]; then
     cp ../tests/hwsim/example-hostapd.config .config
+else
+    echo "hostapd config file exists"
 fi
+
+if [ $use_lcov -eq 1 ]; then
+    if ! grep -q CONFIG_CODE_COVERAGE .config; then
+	    echo CONFIG_CODE_COVERAGE=y >> .config
+    else
+	    echo "CONFIG_CODE_COVERAGE already exists in hostapd/.config. Ignore"
+    fi
+fi
+
 make clean
 make -j8 hostapd hlr_auc_gw
 cd ../wlantest

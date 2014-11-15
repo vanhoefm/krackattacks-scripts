edfefaed3c89c48a9694173ee1bf902bb94c3769
Masashi Honma <masashi.honma@gmail.com>
2014-11-15 10:35:25 +0900

    mesh: Set driver capability flags to mesh interface
    Signed-off-by: Kenzoh Nishikawa <Kenzoh.Nishikawa@jp.sony.com>
    Signed-off-by: Masashi Honma <masashi.honma@gmail.com>

--- wpa_supplicant/mesh.c
+++ wpa_supplicant/mesh.c
@@ -134,6 +134,7 @@ static int wpa_supplicant_mesh_init(struct wpa_supplicant *wpa_s,
 	if (!ifmsh)
 		return -ENOMEM;
 
+	ifmsh->drv_flags = wpa_s->drv_flags;
 	ifmsh->num_bss = 1;
 	ifmsh->bss = os_calloc(wpa_s->ifmsh->num_bss,
 			       sizeof(struct hostapd_data *));

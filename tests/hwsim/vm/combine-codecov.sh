13113d127c032fec3a37dbbd004e92180716800a
Jouni Malinen <j@w1.fi>
2014-12-29 17:22:17 +0200

    tests: Filter out extra files from codecov reports
    wpa_cli and hostapd_cli are not currently tested for code coverage, so
    filter the files specific to those components away from the code
    coverage reports. *_module_tests.c are not included in normal builds, so
    drop them as well. In addition, drop the system header file (byteswap.h)
    that gets somehow unnecessarily included in the reports for couple of
    lines.
    
    Signed-off-by: Jouni Malinen <j@w1.fi>

--- tests/hwsim/vm/combine-codecov.sh
+++ tests/hwsim/vm/combine-codecov.sh
@@ -17,8 +17,16 @@ for i in lcov-*.info-*; do
 done
 
 lcov $args -o $LOGDIR/combined.info > $LOGDIR/combined-lcov.log 2>&1
+cat $LOGDIR/combined.info |
+    sed "/^TN:$/{N;s/TN:\n\(SF:.*\/bits\/byteswap.h$\)/\1/};/^SF:.*\/bits\/byteswap.h$/,/^end_of_record$/d" |
+    sed "/^TN:$/{N;s/TN:\n\(SF:.*\/common\/wpa_ctrl.c$\)/\1/};/^SF:.*\/common\/wpa_ctrl.c$/,/^end_of_record$/d" |
+    sed "/^TN:$/{N;s/TN:\n\(SF:.*\/utils\/edit.c$\)/\1/};/^SF:.*\/utils\/edit.c$/,/^end_of_record$/d" |
+    sed "/^TN:$/{N;s/TN:\n\(SF:.*_module_tests.c$\)/\1/};/^SF:.*_module_tests.c$/,/^end_of_record$/d" |
+    sed "/^TN:$/{N;s/TN:\n\(SF:.*\/hostapd\/hostapd_cli.c$\)/\1/};/^SF:.*\/hostapd\/hostapd_cli.c$/,/^end_of_record$/d" |
+    sed "/^TN:$/{N;s/TN:\n\(SF:.*wpa_supplicant\/wpa_cli.c$\)/\1/};/^SF:.*wpa_supplicant\/wpa_cli.c$/,/^end_of_record$/d" > $LOGDIR/combined.info.filtered
+
 cd $LOGDIR
-genhtml -t "wpa_supplicant/hostapd combined for hwsim test run $(date +%s)" combined.info --output-directory $ODIR > lcov.log 2>&1
+genhtml -t "wpa_supplicant/hostapd combined for hwsim test run $(date +%s)" combined.info.filtered --output-directory $ODIR > lcov.log 2>&1
 
 rm -r /tmp/logs/alt-wpa_supplicant
 rm -r /tmp/logs/alt-hostapd

c975e1c1da5f2345b31b67ea339764d69a0c3e9e
Jouni Malinen <j@w1.fi>
2015-01-05 18:35:14 +0200

    tests: Additional bgscan coverage
    Verify error cases in bgscan module parameters. Increase learn bgscan
    module coverage with parsing of the saved data and probe frequency
    selection.
    
    Signed-off-by: Jouni Malinen <j@w1.fi>

--- tests/hwsim/test_bgscan.py
+++ tests/hwsim/test_bgscan.py
@@ -21,6 +21,26 @@ def test_bgscan_simple(dev, apdev):
     dev[1].connect("bgscan", key_mgmt="NONE", scan_freq="2412",
                    bgscan="simple:1:-45:2")
 
+    dev[2].connect("bgscan", key_mgmt="NONE", scan_freq="2412",
+                   bgscan="simple:1:-45")
+    dev[2].request("REMOVE_NETWORK all")
+    dev[2].wait_disconnected()
+
+    dev[2].connect("bgscan", key_mgmt="NONE", scan_freq="2412",
+                   bgscan="simple:0:0")
+    dev[2].request("REMOVE_NETWORK all")
+    dev[2].wait_disconnected()
+
+    dev[2].connect("bgscan", key_mgmt="NONE", scan_freq="2412",
+                   bgscan="simple")
+    dev[2].request("REMOVE_NETWORK all")
+    dev[2].wait_disconnected()
+
+    dev[2].connect("bgscan", key_mgmt="NONE", scan_freq="2412",
+                   bgscan="simple:1")
+    dev[2].request("REMOVE_NETWORK all")
+    dev[2].wait_disconnected()
+
     ev = dev[0].wait_event(["CTRL-EVENT-SIGNAL-CHANGE"], timeout=10)
     if ev is None:
         raise Exception("dev0 did not indicate signal change event")
@@ -61,8 +81,28 @@ def test_bgscan_learn(dev, apdev):
     try:
         dev[0].connect("bgscan", key_mgmt="NONE", scan_freq="2412",
                        bgscan="learn:1:-20:2")
-        dev[1].connect("bgscan", key_mgmt="NONE", scan_freq="2412",
-                       bgscan="learn:1:-45:2:/tmp/test_bgscan_learn.bgscan")
+        id = dev[1].connect("bgscan", key_mgmt="NONE", scan_freq="2412",
+                            bgscan="learn:1:-45:2:/tmp/test_bgscan_learn.bgscan")
+
+        dev[2].connect("bgscan", key_mgmt="NONE", scan_freq="2412",
+                       bgscan="learn:1:-45")
+        dev[2].request("REMOVE_NETWORK all")
+        dev[2].wait_disconnected()
+
+        dev[2].connect("bgscan", key_mgmt="NONE", scan_freq="2412",
+                       bgscan="learn:0:0")
+        dev[2].request("REMOVE_NETWORK all")
+        dev[2].wait_disconnected()
+
+        dev[2].connect("bgscan", key_mgmt="NONE", scan_freq="2412",
+                       bgscan="learn")
+        dev[2].request("REMOVE_NETWORK all")
+        dev[2].wait_disconnected()
+
+        dev[2].connect("bgscan", key_mgmt="NONE", scan_freq="2412",
+                       bgscan="learn:1")
+        dev[2].request("REMOVE_NETWORK all")
+        dev[2].wait_disconnected()
 
         ev = dev[0].wait_event(["CTRL-EVENT-SIGNAL-CHANGE"], timeout=10)
         if ev is None:
@@ -94,7 +134,6 @@ def test_bgscan_learn(dev, apdev):
         dev[0].request("DISCONNECT")
         dev[1].request("DISCONNECT")
         dev[0].request("REMOVE_NETWORK all")
-        dev[1].request("REMOVE_NETWORK all")
 
         with open("/tmp/test_bgscan_learn.bgscan", "r") as f:
             lines = f.read().splitlines()
@@ -108,6 +147,19 @@ def test_bgscan_learn(dev, apdev):
             raise Exception("Missing BSS1->BSS2 neighbor entry")
         if 'NEIGHBOR 02:00:00:00:04:00 02:00:00:00:03:00' not in lines:
             raise Exception("Missing BSS2->BSS1 neighbor entry")
+
+        dev[1].set_network(id, "scan_freq", "")
+        dev[1].connect_network(id)
+
+        ev = dev[1].wait_event(["CTRL-EVENT-SCAN-STARTED"], timeout=10)
+        if ev is None:
+            raise Exception("dev1 did not start a scan")
+
+        ev = dev[1].wait_event(["CTRL-EVENT-SCAN-RESULTS"], 10)
+        if ev is None:
+            raise Exception("dev1 did not complete a scan")
+
+        dev[1].request("REMOVE_NETWORK all")
     finally:
         try:
             os.remove("/tmp/test_bgscan_learn.bgscan")

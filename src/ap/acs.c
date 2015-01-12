1648cc642711bdb678c8c1d14344b80df2237d6b
Adrien Decostre <ad.decostre@gmail.com>
2015-01-12 08:57:17 +0100

    ACS: Allow subset of channels to be configured
    Add the possibility to define a subset of channels used by the ACS
    engine when not operating on DFS channels.
    
    Signed-off-by: Adrien Decostre <ad.decostre@gmail.com>

--- src/ap/acs.c
+++ src/ap/acs.c
@@ -455,6 +455,22 @@ static int acs_usable_chan(struct hostapd_channel_data *chan)
 }
 
 
+static int is_in_chanlist(struct hostapd_iface *iface,
+			  struct hostapd_channel_data *chan)
+{
+	int *entry;
+
+	if (!iface->conf->chanlist)
+		return 1;
+
+	for (entry = iface->conf->chanlist; *entry != -1; entry++) {
+		if (*entry == chan->chan)
+			return 1;
+	}
+	return 0;
+}
+
+
 static void acs_survey_all_chans_intereference_factor(
 	struct hostapd_iface *iface)
 {
@@ -465,6 +481,9 @@ static void acs_survey_all_chans_intereference_factor(
 		chan = &iface->current_mode->channels[i];
 
 		if (!acs_usable_chan(chan))
+			continue;
+
+		if (!is_in_chanlist(iface, chan))
 			continue;
 
 		wpa_printf(MSG_DEBUG, "ACS: Survey analysis for channel %d (%d MHz)",
@@ -543,6 +562,8 @@ acs_find_ideal_chan(struct hostapd_iface *iface)
 		if (chan->flag & HOSTAPD_CHAN_DISABLED)
 			continue;
 
+		if (!is_in_chanlist(iface, chan))
+			continue;
 
 		/* HT40 on 5 GHz has a limited set of primary channels as per
 		 * 11n Annex J */

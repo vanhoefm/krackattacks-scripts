2acd2985c6bc4a6c3e518738e406215b424bb39c
Jouni Malinen <jouni@qca.qualcomm.com>
2015-02-03 16:30:27 +0200

    tests: P2P_PROV_DISC error case
    Signed-off-by: Jouni Malinen <jouni@qca.qualcomm.com>

--- tests/hwsim/test_p2p_discovery.py
+++ tests/hwsim/test_p2p_discovery.py
@@ -86,6 +86,8 @@ def test_discovery(dev):
         raise Exception("Invalid P2P_PROV_DISC accepted")
     if "FAIL" not in dev[0].global_request("P2P_PROV_DISC 00:11:22:33:44:55 pbc join"):
         raise Exception("Invalid P2P_PROV_DISC accepted")
+    if "FAIL" not in dev[0].global_request("P2P_PROV_DISC 00:11:22:33:44:55 foo"):
+        raise Exception("Invalid P2P_PROV_DISC accepted")
 
 def test_discovery_pd_retries(dev):
     """P2P device discovery and provision discovery retries"""

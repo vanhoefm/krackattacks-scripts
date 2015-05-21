e1ede80d3b96daafe09aca9b98d4f82c3e5b410f
Ondřej Caletka <ondrej@caletka.cz>
2015-05-21 16:24:56 +0200

    eapol_test: Support IPv6 for authentication server
    This allows testing RADIUS servers over IPv6.
    
    Signed-off-by: Ondřej Caletka <ondrej@caletka.cz>

--- wpa_supplicant/eapol_test.c
+++ wpa_supplicant/eapol_test.c
@@ -938,13 +938,12 @@ static void wpa_init_conf(struct eapol_test_data *e,
 		*pos++ = a[3];
 	}
 #else /* CONFIG_NATIVE_WINDOWS or CONFIG_ANSI_C_EXTRA */
-	if (inet_aton(authsrv, &as->addr.u.v4) < 0) {
+	if (hostapd_parse_ip_addr(authsrv, &as->addr) < 0) {
 		wpa_printf(MSG_ERROR, "Invalid IP address '%s'",
 			   authsrv);
 		assert(0);
 	}
 #endif /* CONFIG_NATIVE_WINDOWS or CONFIG_ANSI_C_EXTRA */
-	as->addr.af = AF_INET;
 	as->port = port;
 	as->shared_secret = (u8 *) os_strdup(secret);
 	as->shared_secret_len = os_strlen(secret);

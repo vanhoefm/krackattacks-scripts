05a90d78dc7218b36c6202d9096af300310bf54f
Jouni Malinen <j@w1.fi>
2015-07-01 23:51:31 +0300

    FT: Allow CCMP-256 and GCMP-256 as group ciphers
    The FT-specific check for valid group cipher in wpa_ft_gen_req_ies() was
    not up-to-date with the current list of supported ciphers. Fix this by
    using a generic function to determine validity of the cipher. In
    practice, this adds support for using CCMP-256 and GCMP-256 as the group
    cipher with FT.
    
    Signed-off-by: Jouni Malinen <j@w1.fi>

--- src/common/wpa_common.c
+++ src/common/wpa_common.c
@@ -492,7 +492,7 @@ static int rsn_key_mgmt_to_bitfield(const u8 *s)
 }
 
 
-static int wpa_cipher_valid_group(int cipher)
+int wpa_cipher_valid_group(int cipher)
 {
 	return wpa_cipher_valid_pairwise(cipher) ||
 		cipher == WPA_CIPHER_GTK_NOT_USED;
--- src/common/wpa_common.h
+++ src/common/wpa_common.h
@@ -435,6 +435,7 @@ int wpa_ft_parse_ies(const u8 *ies, size_t ies_len, struct wpa_ft_ies *parse);
 int wpa_cipher_key_len(int cipher);
 int wpa_cipher_rsc_len(int cipher);
 int wpa_cipher_to_alg(int cipher);
+int wpa_cipher_valid_group(int cipher);
 int wpa_cipher_valid_pairwise(int cipher);
 int wpa_cipher_valid_mgmt_group(int cipher);
 u32 wpa_cipher_to_suite(int proto, int cipher);

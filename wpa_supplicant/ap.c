d7ded54774410591abd5705eb203455baf19a7de
Andrei Otcheretianski <andrei.otcheretianski@intel.com>
2015-09-08 12:46:10 +0300

    Update wpa_s->current_ssid->frequency on CS event
    Update wpa_s->current_ssid->frequency when EVENT_CH_SWITCH is received.
    
    Signed-off-by: Andrei Otcheretianski <andrei.otcheretianski@intel.com>

--- wpa_supplicant/ap.c
+++ wpa_supplicant/ap.c
@@ -1205,6 +1205,8 @@ void wpas_ap_ch_switch(struct wpa_supplicant *wpa_s, int freq, int ht,
 		return;
 
 	wpa_s->assoc_freq = freq;
+	if (wpa_s->current_ssid)
+		wpa_s->current_ssid->frequency = freq;
 	hostapd_event_ch_switch(wpa_s->ap_iface->bss[0], freq, ht, offset, width, cf1, cf1);
 }
 

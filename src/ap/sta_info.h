9b285081ff1812124b40a6821d953d99fe32509c
Dmitry Ivanov <dmitrijs.ivanovs@ubnt.com>
2015-10-06 16:26:58 +0300

    Wait longer for inactive client probe (empty data frame)
    Some devices cannot respond to inactive client probe (empty data frame)
    within one second. For example, iPhone may take up to 3 secs. This
    becomes a significant problem when ap_max_inactivity is set to lower
    value such as 10 secs. iPhone can lose Wi-Fi connection after ~1 min
    of user inactivity.
    
    Signed-off-by: Dmitry Ivanov <dima@ubnt.com>

--- src/ap/sta_info.h
+++ src/ap/sta_info.h
@@ -181,7 +181,7 @@ struct sta_info {
  * AP_DISASSOC_DELAY seconds. Similarly, the station will be deauthenticated
  * after AP_DEAUTH_DELAY seconds has passed after disassociation. */
 #define AP_MAX_INACTIVITY (5 * 60)
-#define AP_DISASSOC_DELAY (1)
+#define AP_DISASSOC_DELAY (3)
 #define AP_DEAUTH_DELAY (1)
 /* Number of seconds to keep STA entry with Authenticated flag after it has
  * been disassociated. */

b94fff1d93e894b55656dea1a833b33b93e8c4f0
Lior David <qca_liord@qca.qualcomm.com>
2016-01-16 18:57:54 +0200

    wpa_supplicant: Configurable EAP fragment size in AP mode
    In wpa_supplicant AP mode, allow configuration of the EAP fragment size
    using the fragment_size member of network block (wpa_ssid), similar to
    the fragment_size in hostapd configuration. bss->fragment_size default
    value of 0 is treated specially in some EAP code paths (such as MTU
    initialization in eap_pwd_init). In order to preserve the existing
    behavior, bss->fragment_size will only be set if the network block
    specified a value different from the default which is
    DEFAULT_FRAGMENT_SIZE(1398) bytes.
    
    Signed-off-by: Lior David <qca_liord@qca.qualcomm.com>

--- wpa_supplicant/ap.c
+++ wpa_supplicant/ap.c
@@ -450,6 +450,8 @@ static int wpa_supplicant_conf_ap(struct wpa_supplicant *wpa_s,
 		os_memcpy(bss->uuid, wpa_s->conf->uuid, WPS_UUID_LEN);
 	os_memcpy(bss->os_version, wpa_s->conf->os_version, 4);
 	bss->pbc_in_m1 = wpa_s->conf->pbc_in_m1;
+	if (ssid->eap.fragment_size != DEFAULT_FRAGMENT_SIZE)
+		bss->fragment_size = ssid->eap.fragment_size;
 no_wps:
 #endif /* CONFIG_WPS */
 

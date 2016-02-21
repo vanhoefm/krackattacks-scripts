f5ca1766dc93f8fb65efdd43670f1d7a74dba581
Michael Braun <michael-dev@fami-braun.de>
2016-02-21 12:01:39 +0100

    VLAN: Fix vlan_compare() for tagged VLANs
    While refactoring VLAN comparison into vlan_compare(), it was overlooked
    that modifications are needed for tagged VLAN support.
    
    Signed-off-by: Michael Braun <michael-dev@fami-braun.de>

--- src/ap/vlan.c
+++ src/ap/vlan.c
@@ -16,6 +16,7 @@
  */
 int vlan_compare(struct vlan_description *a, struct vlan_description *b)
 {
+	int i;
 	const int a_empty = !a || !a->notempty;
 	const int b_empty = !b || !b->notempty;
 
@@ -25,5 +26,9 @@ int vlan_compare(struct vlan_description *a, struct vlan_description *b)
 		return 1;
 	if (a->untagged != b->untagged)
 		return 1;
+	for (i = 0; i < MAX_NUM_TAGGED_VLAN; i++) {
+		if (a->tagged[i] != b->tagged[i])
+			return 1;
+	}
 	return 0;
 }

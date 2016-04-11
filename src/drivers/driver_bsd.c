18ae3a675cd56ebcf476c232c603dd6da31e8441
Roy Marples <roy@marples.name>
2016-04-11 09:56:20 +0100

    bsd: Set level correctly for non FreeBSD systems
    Only FreeBSD treats rssi as dBm, other BSD have no special meaning to
    rssi.
    
    Signed-off-by: Roy Marples <roy@marples.name>

--- src/drivers/driver_bsd.c
+++ src/drivers/driver_bsd.c
@@ -1376,11 +1376,16 @@ wpa_driver_bsd_add_scan_entry(struct wpa_scan_results *res,
 	result->caps = sr->isr_capinfo;
 	result->qual = sr->isr_rssi;
 	result->noise = sr->isr_noise;
+
+#ifdef __FreeBSD__
 	/*
 	 * the rssi value reported by the kernel is in 0.5dB steps relative to
 	 * the reported noise floor. see ieee80211_node.h for details.
 	 */
 	result->level = sr->isr_rssi / 2 + sr->isr_noise;
+#else
+	result->level = sr->isr_rssi;
+#endif
 
 	pos = (u8 *)(result + 1);
 

0335ff7f69046b1d0acfea2621d0311a19408646
Jonathan Afek <jonathan@wizery.com>
2016-05-19 16:06:48 +0300

    tests/remote: Fix usage of a non existing variable
    In monitor.py in the remote tests code there is fucntion create() that
    creates standalone monitor interfaces. In this function there is an
    iteration of the ifaces of the host by using the ifaces variable but
    this variable is non-existing. This patch creates this variable before
    its usage.
    
    Signed-off-by: Jonathan Afek <jonathanx.afek@intel.com>

--- tests/remote/monitor.py
+++ tests/remote/monitor.py
@@ -37,6 +37,7 @@ def create(devices, setup_params, refs, duts, monitors):
         try:
             host.execute(["iw", "reg", "set", setup_params['country']])
             setup_hw = setup_params['setup_hw']
+            ifaces = re.split('; | |, ', host.ifname)
             for iface in ifaces:
                 host.execute(setup_hw + " -I " + iface + " -R 1")
         except:

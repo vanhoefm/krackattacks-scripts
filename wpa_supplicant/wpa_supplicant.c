331f07742f622ed6bd0019dcb574735fbbbfddde
Jouni Malinen <j@w1.fi>
2016-06-04 21:30:51 +0300

    mesh: Allow 160 MHz channel to be configured
    This allows minimal testing with 160 MHz channel with country code ZA
    that happens to be the only one with a non-DFS 160 MHz frequency. DFS
    with mesh is not yet supported.
    
    Signed-off-by: Jouni Malinen <j@w1.fi>

--- wpa_supplicant/wpa_supplicant.c
+++ wpa_supplicant/wpa_supplicant.c
@@ -2039,6 +2039,16 @@ void ibss_mesh_setup_freq(struct wpa_supplicant *wpa_s,
 			if (chwidth == VHT_CHANWIDTH_80P80MHZ)
 				break;
 		}
+	} else if (ssid->max_oper_chwidth == VHT_CHANWIDTH_160MHZ) {
+		if (freq->freq == 5180) {
+			chwidth = VHT_CHANWIDTH_160MHZ;
+			vht_caps |= VHT_CAP_SUPP_CHAN_WIDTH_160MHZ;
+			seg0 = 50;
+		} else if (freq->freq == 5520) {
+			chwidth = VHT_CHANWIDTH_160MHZ;
+			vht_caps |= VHT_CAP_SUPP_CHAN_WIDTH_160MHZ;
+			seg0 = 114;
+		}
 	}
 
 	if (hostapd_set_freq_params(&vht_freq, mode->mode, freq->freq,

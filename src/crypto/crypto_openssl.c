0d42179e1246f996d334c8bd18deca469fdb1add
Christian Neukirchen <chneukirchen@gmail.com>
2016-10-05 16:35:48 +0200

    LibreSSL: Fix dh5 code
    Add LibreSSL check to old OpenSSL #ifdef guard as DH_{get0,set0}_key()
    is not implemented in LibreSSL.
    
    Signed-off-by: Christian Neukirchen <chneukirchen@gmail.com>

--- src/crypto/crypto_openssl.c
+++ src/crypto/crypto_openssl.c
@@ -611,7 +611,7 @@ void crypto_cipher_deinit(struct crypto_cipher *ctx)
 
 void * dh5_init(struct wpabuf **priv, struct wpabuf **publ)
 {
-#if OPENSSL_VERSION_NUMBER < 0x10100000L
+#if OPENSSL_VERSION_NUMBER < 0x10100000L || defined(LIBRESSL_VERSION_NUMBER)
 	DH *dh;
 	struct wpabuf *pubkey = NULL, *privkey = NULL;
 	size_t publen, privlen;
@@ -712,7 +712,7 @@ err:
 
 void * dh5_init_fixed(const struct wpabuf *priv, const struct wpabuf *publ)
 {
-#if OPENSSL_VERSION_NUMBER < 0x10100000L
+#if OPENSSL_VERSION_NUMBER < 0x10100000L || defined(LIBRESSL_VERSION_NUMBER)
 	DH *dh;
 
 	dh = DH_new();

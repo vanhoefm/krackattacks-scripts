73a27a6345362f17bee1fb5c52baec560a530fbe
Jouni Malinen <j@w1.fi>
2017-01-02 00:29:30 +0200

    Add hostapd configuration parameter rrm_beacon_report
    rrm_beacon_report=1 can now be used to make hostapd advertise capability
    for beacon reports (passive, active, table). Actual mechanism for
    sending out beacon requests will be added in separate commits.
    
    Signed-off-by: Jouni Malinen <j@w1.fi>

--- hostapd/config_file.c
+++ hostapd/config_file.c
@@ -3589,6 +3589,12 @@ static int hostapd_config_fill(struct hostapd_config *conf,
 		if (atoi(pos))
 			bss->radio_measurements[0] |=
 				WLAN_RRM_CAPS_NEIGHBOR_REPORT;
+	} else if (os_strcmp(buf, "rrm_beacon_report") == 0) {
+		if (atoi(pos))
+			bss->radio_measurements[0] |=
+				WLAN_RRM_CAPS_BEACON_REPORT_PASSIVE |
+				WLAN_RRM_CAPS_BEACON_REPORT_ACTIVE |
+				WLAN_RRM_CAPS_BEACON_REPORT_TABLE;
 	} else if (os_strcmp(buf, "gas_address3") == 0) {
 		bss->gas_address3 = atoi(pos);
 	} else if (os_strcmp(buf, "stationary_ap") == 0) {

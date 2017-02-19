e01cf2afc948222963fd182ca271b79ac7389dd3
Jouni Malinen <j@w1.fi>
2017-02-19 12:28:33 +0200

    Define eapol_sm_get_eap_proxy_imsi() only with CONFIG_EAP_PROXY=y
    This function is called only from locations within ifdef
    CONFIG_EAP_PROXY, so there is no need to try to cover the not-defined
    case here and the function can simply be removed completely if
    CONFIG_EAP_PROXY=y is not used.
    
    Signed-off-by: Jouni Malinen <j@w1.fi>

--- src/eapol_supp/eapol_supp_sm.c
+++ src/eapol_supp/eapol_supp_sm.c
@@ -2155,16 +2155,14 @@ int eapol_sm_failed(struct eapol_sm *sm)
 }
 
 
+#ifdef CONFIG_EAP_PROXY
 int eapol_sm_get_eap_proxy_imsi(struct eapol_sm *sm, char *imsi, size_t *len)
 {
-#ifdef CONFIG_EAP_PROXY
 	if (sm->eap_proxy == NULL)
 		return -1;
 	return eap_proxy_get_imsi(sm->eap_proxy, imsi, len);
-#else /* CONFIG_EAP_PROXY */
-	return -1;
-#endif /* CONFIG_EAP_PROXY */
 }
+#endif /* CONFIG_EAP_PROXY */
 
 
 void eapol_sm_erp_flush(struct eapol_sm *sm)

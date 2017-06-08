3f8e3a5486a9fa5e0d37021c47f8f673d32867c8
Naftali Goldstein <naftali.goldstein@intel.com>
2017-06-08 11:17:55 +0300

    ap: Fix invalid HT40 channel pair fallback
    In case of incorrect HT40 configuration as part of an attempt to create
    a 80 MHz AP, iface->conf->vht_oper_centr_freq_seg0_idx and
    iface->conf->vht_oper_centr_freq_seg1_idx are zero'ed, but
    iface->conf->vht_oper_chwidth remains VHT_CHANWIDTH_80MHZ. This causes
    the logic in dfs_get_start_chan_idx to fail.
    
    Fix this by setting iface->conf->vht_oper_chwidth to
    VHT_CHANWIDTH_USE_HT when zero'ing the center frequency parameters.
    
    Signed-off-by: Naftali Goldstein <naftali.goldstein@intel.com>

--- src/ap/hw_features.c
+++ src/ap/hw_features.c
@@ -334,6 +334,7 @@ static void ieee80211n_check_scan(struct hostapd_iface *iface)
 		iface->conf->secondary_channel = 0;
 		iface->conf->vht_oper_centr_freq_seg0_idx = 0;
 		iface->conf->vht_oper_centr_freq_seg1_idx = 0;
+		iface->conf->vht_oper_chwidth = VHT_CHANWIDTH_USE_HT;
 		res = 1;
 		wpa_printf(MSG_INFO, "Fallback to 20 MHz");
 	}

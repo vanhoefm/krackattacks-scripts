44d6b272cf92b7d3da08270e3d7a8cd9a1f2f867
Jouni Malinen <jouni@qca.qualcomm.com>
2017-07-02 12:36:15 +0300

    DPP: Fix configuration item list
    This was supposed to use semicolons, not commas..
    
    Signed-off-by: Jouni Malinen <jouni@qca.qualcomm.com>

--- wpa_supplicant/config_file.c
+++ wpa_supplicant/config_file.c
@@ -850,11 +850,11 @@ static void wpa_config_write_network(FILE *f, struct wpa_ssid *ssid)
 	INT(group_rekey);
 	INT(ignore_broadcast_ssid);
 #ifdef CONFIG_DPP
-	STR(dpp_connector),
-	STR(dpp_netaccesskey),
-	INT(dpp_netaccesskey_expiry),
-	STR(dpp_csign),
-	INT(dpp_csign_expiry),
+	STR(dpp_connector);
+	STR(dpp_netaccesskey);
+	INT(dpp_netaccesskey_expiry);
+	STR(dpp_csign);
+	INT(dpp_csign_expiry);
 #endif /* CONFIG_DPP */
 #ifdef CONFIG_HT_OVERRIDES
 	INT_DEF(disable_ht, DEFAULT_DISABLE_HT);

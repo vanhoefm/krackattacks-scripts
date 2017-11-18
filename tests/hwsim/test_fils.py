8f0831573741854e33d1bf1c4ffc609afd693efb
Jouni Malinen <j@w1.fi>
2017-11-18 13:55:41 +0200

    tests: Skip fils_sk_pfs_25 with BoringSSL
    It looks like BoringSSL has dropped support for group 25 (192-bit Random
    ECP Group).
    
    Signed-off-by: Jouni Malinen <j@w1.fi>

--- tests/hwsim/test_fils.py
+++ tests/hwsim/test_fils.py
@@ -1369,6 +1369,9 @@ def run_fils_sk_pfs(dev, apdev, group, params):
     check_erp_capa(dev[0])
 
     tls = dev[0].request("GET tls_library")
+    if int(group) in [ 25 ]:
+        if not (tls.startswith("OpenSSL") and ("build=OpenSSL 1.0.2" in tls or "build=OpenSSL 1.1" in tls) and ("run=OpenSSL 1.0.2" in tls or "run=OpenSSL 1.1" in tls)):
+            raise HwsimSkip("EC group not supported")
     if int(group) in [ 27, 28, 29, 30 ]:
         if not (tls.startswith("OpenSSL") and ("build=OpenSSL 1.0.2" in tls or "build=OpenSSL 1.1" in tls) and ("run=OpenSSL 1.0.2" in tls or "run=OpenSSL 1.1" in tls)):
             raise HwsimSkip("Brainpool EC group not supported")

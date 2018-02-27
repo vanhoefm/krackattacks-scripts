ba6080441985549008f780cfa5e715d39b3b7d78
Kiran Kumar Lokere <klokere@codeaurora.org>
2018-02-26 18:54:37 -0800

    Add new WiFi test config attribute to allow WEP/TKIP in HE
    Define a new WiFi test configuration attribute in QCA vendor
    command to allow or not to allow WEP/TKIP in HT/VHT/HE mode.
    This is used for configuring the testbed device.
    
    Signed-off-by: Jouni Malinen <jouni@codeaurora.org>

--- src/common/qca-vendor.h
+++ src/common/qca-vendor.h
@@ -5250,6 +5250,13 @@ enum qca_wlan_vendor_attr_wifi_test_config {
 	 */
 	QCA_WLAN_VENDOR_ATTR_WIFI_TEST_CONFIG_HE_MCS = 5,
 
+	/* 8-bit unsigned value to configure the driver to allow or not to
+	 * allow the connection with WEP/TKIP in HT/VHT/HE modes.
+	 * This attribute is used to configure the testbed device.
+	 * 1-allow WEP/TKIP in HT/VHT/HE, 0-do not allow WEP/TKIP.
+	 */
+	QCA_WLAN_VENDOR_ATTR_WIFI_TEST_CONFIG_WEP_TKIP_IN_HE = 6,
+
 	/* keep last */
 	QCA_WLAN_VENDOR_ATTR_WIFI_TEST_CONFIG_AFTER_LAST,
 	QCA_WLAN_VENDOR_ATTR_WIFI_TEST_CONFIG_MAX =
